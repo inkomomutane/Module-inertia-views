f8d655a1e579228e370bbe3a521691e85822fcb4
GitHub Action <action@github.com>
2024-08-14 13:23:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/inkomomutane/moduleinertiaviews/MyBundle.kt
+++ src/main/kotlin/com/github/inkomomutane/moduleinertiaviews/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.inkomomutane.moduleinertiaviews
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/inkomomutane/moduleinertiaviews/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/inkomomutane/moduleinertiaviews/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.inkomomutane.moduleinertiaviews.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
